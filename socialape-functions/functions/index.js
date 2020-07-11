5a98d326892c75ea919be6c5d653f9f48a103d79
Samyak Maharjan <samyakjyapu33127@gmail.com>
2020-07-11 19:59:45 +0545

    add validations for user registrations

--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -51,6 +51,16 @@ app.post("/scream", (req, res) => {
     });
 });
 
+const isEmpty = (string) => {
+  if(string.trim() === '') return true;
+  return false;
+}
+
+const isEmail = (email) => {
+  const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if(email.match(regex)) return true;
+  return false;
+}
 //Signup Route
 app.post("/signup", (req, res) => {
   const newUser = {
@@ -60,7 +70,20 @@ app.post("/signup", (req, res) => {
     handle: req.body.handle,
   };
 
-  //TODO: validate data
+  //validate data
+  let errors = {};
+  if(isEmpty(newUser.email)){
+    errors.email = 'Must not be empty';
+  }else if(!isEmail(newUser.email)){
+    errors.email = 'Must be a valid email address';
+  }
+
+  if(isEmpty(newUser.password)) errors.password = 'Must not be empty';
+  if(newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match';
+  if(isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
+
+  if(Object.keys(errors).length > 0) return res.status(400).json(errors);
+
   let token, userId;
   db.doc(`/users/${newUser.handle}`)
     .get()
